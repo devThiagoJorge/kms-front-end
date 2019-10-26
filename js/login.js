33e1e97da75c96212edee81e2db3ddf00dbd8532
devThiagoJorge <thiago.v.jorge@hotmail.com>
2019-10-26 15:11:32 -0300

    Tela de login (funcional), tela de cadastro (funcional), ambas salvando no local storage

--- js/login.js
+++ js/login.js
@@ -1,24 +1,34 @@
-$('#btnRequest').click(function(){
-    var inputEmail = $('#inputEmail').val();
-    var inputPassword = $('#inputPassword').val(); 
+$("#btnRequest").click(function() {
+  var inputEmail = $("#inputEmail").val();
+  var inputPassword = $("#inputPassword").val();
 
-    axios.post('http://localhost:3001/login/', {email: inputEmail , password: inputPassword})
-    .then(function(response){            
-            if (response.data.error != undefined) {
+  axios
+    .post("http://localhost:3001/login/", {
+      email: inputEmail,
+      password: inputPassword
+    })
+    .then(function(response) {
+      if (response.data.error != undefined) {
+        if (response.data.errorId == "1") {
+          //Troca cor do campo email para vermelho
+          alert("E-mail inválido.");
+        } else {
+          //Troca cor do campo senha para vermelho
+          alert("Senha inválida.");
+        }
+      } else {
+        window.location.href = "perfil.html";
 
-                if (response.data.errorId == '1') {
-                    //Troca cor do campo email para vermelho
-                    alert('E-mail inválido.');
-                } else {
-                    //Troca cor do campo senha para vermelho
-                    alert('Senha inválida.');
-                }
-            }else{
-               
-                alert('Bien viendo!');        
-            }      
-        })
-        .catch(function(error){
-            alert(error);        
-        }); 
+     
+        var obj = response.data.user;
+
+        Object.keys(obj).forEach(function(item) {
+          localStorage.setItem(item, obj[item]);
+        });
+            localStorage.setItem('token', response.data.token);
+      }
+    })
+    .catch(function(error) {
+      alert(error);
+    });
 });
