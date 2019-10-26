25d60bb3811a1ad3eb1c78ca6a4c18babcde1b6a
devThiagoJorge <thiago.v.jorge@hotmail.com>
2019-10-26 12:37:42 -0300

    Tela de cadastro + API CEP, em funcionamento

--- js/login.js
+++ js/login.js
@@ -2,7 +2,6 @@ $('#btnRequest').click(function(){
     var inputEmail = $('#inputEmail').val();
     var inputPassword = $('#inputPassword').val(); 
 
-    console.log(inputEmail, inputPassword);
     axios.post('http://localhost:3001/login/', {email: inputEmail , password: inputPassword})
     .then(function(response){            
             if (response.data.error != undefined) {
@@ -15,11 +14,11 @@ $('#btnRequest').click(function(){
                     alert('Senha inválida.');
                 }
             }else{
-                console.log(response.data);
+               
                 alert('Bien viendo!');        
             }      
         })
         .catch(function(error){
-            console.log(error);        
+            alert(error);        
         }); 
 });
--- js/signup.js
+++ js/signup.js
@@ -1,22 +1,80 @@
 $(document).ready(function () {
-    $("#inputHomePhone, #inputCellphone").mask("(00) 0000-0000");
+    $(" #inputCellphone").mask("(00) 0000-00000");
+});
+$(document).ready(function () {
+    $("#inputHomePhone").mask("(00) 0000-0000");
 });
 $(document).ready(function () {
     $("#inputCep").mask("00000-000");
 });
 
-
-$('#inputCep').focusout(function () {
+$('#inputCep').keyup(function () {
     var cep = $('#inputCep').val();
-    axios.get('https://viacep.com.br/ws/' + cep + '/json/')
-        .then(function (response) {
-            var state = response.data.uf;
-            var city = response.data.localidade;
-            $('#inputState').val(state);
-            $('#inputCity').val(city);
+    if(cep.length === 9){
+        axios.get('https://viacep.com.br/ws/' + cep + '/json/')
+        .then(function (response) {      
+            if(!(response.data.erro)){
+                var state = response.data.uf;
+                var city = response.data.localidade;
+                $('#inputState').val(state);
+                $('#inputCity').val(city);
+            }
+            else{
+                alert("Cep não encontrado");
+                $('#inputCep').val("");
+            }
         })
         .catch(function (error) {
             console.log(error);
         })
+
+    }
+   
 });
 
+$('#btnRegister').click(function(){
+    var inputEmail = $('#inputEmail').val();
+    var inputPassword  = $('#inputPassword').val(); 
+    var inputFirstName = $('#inputFirstName').val();
+    var inputLastName  = $('#inputLastName').val();
+    var inputCellPhone = $("#inputCellphone").val();
+    var inputHomePhone = $("#inputHomePhone").val();
+    var inputCep = $('#inputCep').val();
+    var inputState = $("#inputState").val();
+    var inputCity = $('#inputCity').val();
+
+
+    axios.post('http://localhost:3001/signup/', 
+    {
+        
+        firstName: inputFirstName,
+        lastName: inputLastName,
+        email: inputEmail, 
+        password: inputPassword, 
+        cellPhone: inputCellPhone,
+        homePhone: inputHomePhone,
+        cep: inputCep,
+        state: inputState,
+        city: inputCity
+    })
+  
+    .then(function(response){            
+        console.log(response.data);
+            if (response.data.error != undefined) {
+
+                if (response.data.errorId == '1') {
+                    //Troca cor do campo email para vermelho
+                    alert('Usuário já cadastrado');
+                } else {
+                    //Troca cor do campo senha para vermelho
+                    alert('Erro ao cadastra usuario.');
+                }
+            }else{
+                console.log(response.data);
+                alert('Bien viendo!');        
+            }      
+        })
+        .catch(function(error){
+            console.log(error);        
+        }); 
+});
