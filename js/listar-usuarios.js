0dfaa14496b4143c11a613f981a0e575ba3b49fe
devThiagoJorge <thiago.v.jorge@hotmail.com>
2019-11-18 15:43:36 -0300

    Paginação funcionando v1

--- js/listar-usuarios.js
+++ js/listar-usuarios.js
@@ -1,81 +1,136 @@
-
-$("#btnSearch").click(function () {
-    searchUser(1);
+$("#btnSearch").click(function() {
+  searchUser(1);
 });
 
 function splitReturnFirstName(firstName) {
-    var inputSearch = $("#inputSearch").val();
-    var stringSplit = inputSearch.split(" ");
-    firstName = stringSplit[0];
-    return firstName;
+  var inputSearch = $("#inputSearch").val();
+  var stringSplit = inputSearch.split(" ");
+  firstName = stringSplit[0];
+  return firstName;
 }
 function splitReturnLastName(lastName) {
-    var inputSearch = $("#inputSearch").val();
-    var stringSplit = inputSearch.split(" ");
+  var inputSearch = $("#inputSearch").val();
+  var stringSplit = inputSearch.split(" ");
 
-    var tamanho = stringSplit.length;
-    tamanho = tamanho - 1;
+  var tamanho = stringSplit.length;
+  tamanho = tamanho - 1;
 
-    lastName = stringSplit[tamanho];
-    if (tamanho == 0 || lastName == '') {
-        lastName = undefined;
-    }
-    return lastName;
+  lastName = stringSplit[tamanho];
+  if (tamanho == 0 || lastName == "") {
+    lastName = undefined;
+  }
+  return lastName;
 }
 
 function searchUser(page) {
-    $("#lista").empty();
-    var firstName, lastName;
-    firstName = splitReturnFirstName(firstName);
-    lastName = splitReturnLastName(lastName);
-    var q = '&firstName=' + firstName;
-    if (lastName != undefined) {
-        q = q + "&lastName=" + lastName;
-    }
-    // var page = 1;
+  empty();
+  var firstName, lastName;
+  firstName = splitReturnFirstName(firstName);
+  lastName = splitReturnLastName(lastName);
+  var q = "&firstName=" + firstName;
+  if (lastName != undefined) {
+    q = q + "&lastName=" + lastName;
+  }
 
-    console.log(page);
-    axios.get("http://localhost:3001/user/search?page=" + page + q)
-        .then(function (response) {
-            var obj = response.data.users.docs;
-            console.log(obj);
-            var tamanho = obj.length;
-            for (var i = 0; i < tamanho; i++) {
-                obj = response.data.users.docs[i];
-                $("#lista").append(
-                    '<tr>' +
-                    '<td>' + parseInt(i + 1) + '</td>' +
-                    '<td>' + obj.firstName + ' ' + obj.lastName + '</td>' +
-                    '<td>' + obj.email + '</td>' +
-                    '<td>' + obj.cellPhone + '</td>' +
-                    '<td>' + obj.city + '</td>' +
-                    '<td>' + obj.state + '</td>' +
-                    '</tr>'
-                )
-            }
-            var tamanho = response.data.users.total;
-            
-            var quantidadeRegistros = response.data.users.docs.length;
+  axios
+    .get("http://localhost:3001/user/search?page=" + page + q)
+    .then(function(response) {
+      var obj = response.data.users.docs;
+      var tamanho = obj.length;
+      if (tamanho == 0) {
+        alert("Usuário não cadastrado");
+      } else {
+        for (var i = 0; i < tamanho; i++) {
+          obj = response.data.users.docs[i];
+          $("#lista").append(
+            "<tr>" +
+              "<td>" +
+              parseInt(i + 1) +
+              "</td>" +
+              "<td>" +
+              obj.firstName +
+              " " +
+              obj.lastName +
+              "</td>" +
+              "<td>" +
+              obj.email +
+              "</td>" +
+              "<td>" +
+              obj.cellPhone +
+              "</td>" +
+              "<td>" +
+              obj.city +
+              "</td>" +
+              "<td>" +
+              obj.state +
+              "</td>" +
+              "</tr>"
+          );
+        }
+        tamanho = response.data.users.total;
+        console.log(tamanho);
+        var quantidadeRegistros = response.data.users.docs.length;
+        paginacao(tamanho, quantidadeRegistros);
+      }
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+function paginacao(tamanho, quantidadeRegistros) {
+  var tamanhoArray = tamanho / quantidadeRegistros;
+  tamanhoArray = parseInt(tamanhoArray);
+
+  var array = new Array();
+  array = new Array(tamanhoArray);
+
+  if (quantidadeRegistros >= 5) {
+    tamanhoArray++;
     
-            paginacao(tamanho, page, quantidadeRegistros);
-
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-}
-
-
-function paginacao(tamanho, number,quantidadeRegistros) {
-    $("#paginacao").empty();
-   
-    $("#message").empty();
-   
-    for (var i = 0; i < tamanho; i += 7) {
-        $("#paginacao").append('<li class="page-item"><button onclick="searchUser(' + parseInt(number) + ');"  class="page-link">' + parseInt(number) + '</button></li>');
-        number++;
+    for (var i = 1; i <= tamanhoArray; i++) {
+      array[i] = i;
+      $("#paginacao").append(
+        '<li class="page-item"><button onclick="searchUser(' +
+          array[i] +
+          ');"  class="page-link">' +
+          array[i] +
+          "</button></li>"
+      );
     }
-    $("#message").append("Mostrando <b>"+quantidadeRegistros+"</b> resultados de <b>" + tamanho + "</b>");
-
+    $("#message").append(
+      "Mostrando <b>" +
+        quantidadeRegistros +
+        "</b> resultados de <b>" +
+        tamanho +
+        "</b>"
+    );
+  
+  } else {
+    var number = 1;
+    for (var i = 1; i <= tamanhoArray; i+=5) {
+      
+      $("#paginacao").append(
+        '<li class="page-item"><button onclick="searchUser(' +
+          number +
+          ');"  class="page-link">' +
+         number+
+          "</button></li>"       
+      );
+      number++;
+    }
+    $("#message").append(
+      "Mostrando <b>" +
+        quantidadeRegistros +
+        "</b> resultados de <b>" +
+        tamanho +
+        "</b>"
+    );
+  }
 }
 
+function empty() {
+  $("#message").empty();
+  $("#lista").empty();
+  $("#paginacao").empty();
+}
